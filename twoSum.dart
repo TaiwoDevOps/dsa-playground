79db8df828182cfe5c4c37ae3ebcf22453527d9e
Adisa Taiwo Joshua <adisataiwojoshua@gmail.com>
2023-01-11 09:17:01 +0100

    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.

---
+++ twoSum.dart
@@ -0,0 +1,40 @@
+class Solution {
+  List<int> twoSum(List<int> nums, int target) {
+    //hold the array in another variable
+// List<int> temp=[];
+
+    List<int> nums2 = nums;
+
+//loop through the first array
+    for (var i = 0; i < nums.length; i++) {
+      // temp = [i, nums[i]];
+
+      print("loop one $i ${nums[i]}  ");
+
+      //loop through the second array
+      for (var j = 1; j < nums2.length; j++) {
+        //iterate through the lists and see if current value of each
+        //array is equal to target
+        print(" loop two $j ${nums2[j]}");
+
+        if (nums[i] + nums2[j] == target) {
+          // print("answer is ${temp[1]} ${nums2[j]}");
+
+          //if yes, return the index
+          return [i, j];
+        }
+      }
+    }
+
+    //else return an empty array
+
+    return [];
+  }
+}
+
+void main() {
+  final class1 = Solution();
+
+  var res = class1.twoSum([2, 5, 5, 11], 10);
+  print("the array are $res");
+}
